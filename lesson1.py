90f3492f2b980c5abff768b73a0b1a6019d28f46
Alexey Voloshin <alexey.voloshin@raccoongang.com>
2022-04-21 17:53:01 +0300

    Add lesson1.py and lesson2.py

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+# import random
+# import string
+#
+# with open("test_file.txt", "w") as test_file:
+#     for _ in range(10000):
+#         line:str = ""
+#         for _ in range(random.randint(1, 100)):
+#             line += random.choice(string.ascii_letters)
+#         test_file.write(f"{line}\n")
+
+print(f"Hello Loki! Age is 21")
+1 / 0
